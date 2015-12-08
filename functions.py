e910eede2c60022e30d4b336f81b58bcab543a94
afholland <alex.f.holland@gmail.com>
2015-12-08 11:42:09 -0600

    Iso cluster function

--- functions.py
+++ functions.py
@@ -1,4 +1,9 @@
 # ProgrammingTermProject
 #Term project for Spatial Programming 2016.
-
-import os, import arcpy 
+import os
+import arcpy 
+from arcpy.sa import *
+def unsup(inraster):
+  classes=8
+  outunsuper=IsoClusterUnsupervisedClassification(inraster,classes,'','','%outsuper.gsg'%(inraster))
+  
