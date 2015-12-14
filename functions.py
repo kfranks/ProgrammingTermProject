e435c9a0e9f1750172cb57b16b0321d6f88af2c3
Katy <kfranks18@ou.edu>
2015-12-14 17:47:07 -0600

    Update functions.py

--- functions.py
+++ functions.py
@@ -3,7 +3,16 @@
 import os
 import arcpy 
 from arcpy.sa import *
+
+# Perform the unsupervised classification to obtain a .gsg file for the maximum likelihood classification that will be performed next
+# Classes for unsupervised classification: water, grasslands, trees, urban, suburban, mountainous terrain, cropland, barren
 def unsup(inraster):
-  classes=8
-  outunsuper=IsoClusterUnsupervisedClassification(inraster,classes,'','','','%outsuper.gsg'%(inraster))
+  classes = 8
+  outunsuper = IsoClusterUnsupervisedClassification(inraster,classes,'','','','%outunsuper.gsg'%(inraster))
+  
+# Perform the maximum likelihood classification and outputting a confidence raster along with the classification raster. 
+def maxlik(inraster,sigfile): 
+  
+  mlcOut = MLClassify(inraster, sigfile, "", "","", outConfidence,'%outConf.tif'%(outConfidence),'%maxlikeout.tif'%(inraster))
+  
   
