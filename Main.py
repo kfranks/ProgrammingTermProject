da5f5b3344457812d679148cff36e0b337b22437
afholland <alex.f.holland@gmail.com>
2015-12-14 18:10:00 -0600

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python
 # Katy Franks, Alex Holland, Parker Pennington 
-#
+import arcpy
+import functions
+from arpy import env
+env.workspace=r'E:\SP_Landsat'
+filelist=['las_vegas.tif','Lincoln.tif','SLC.tif','Tucsaloosa.tif']
+for x in filelist:
+  functions.unsup(x)
+for x in filelist:
+  gsg=x.split('.')[0]+'.gsg'
+  functions.maxlik(x,gsg)
+  
